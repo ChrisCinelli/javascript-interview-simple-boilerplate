e5d365b959f2a2d8186166f18f8fd1f216fbbb7e
Jyotishman saikia <jyotishmansaikia@Jyotishmans-MacBook-Pro.local>
2019-10-31 01:20:00 +0530

    initial files added for a nodemon server files

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-console.log('---- START ----');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-module.exports = function sampleAdd(a, b) {
-  return a + b;
-}
+var app = express();
+var port = 8000;
 
-console.log('----  END  ----');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/',function(req, res) {
+  res.render("index.html");
+});
+
+app.listen(port, function() {
+    console.log("The http://localhost: " + port + " is listening");
+    console.log('Server is running in', process.env.NODE_ENV + ' mode.....................');
+});
